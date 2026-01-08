4c2630fc3f2ce07970c4ec18d2d65a5e44985348
ynnuarr <ynnuarr77@gmail.com>
2026-01-08 16:13:39 +0800

    tampilan halaman form

--- resources/views/layouts/navbar/header.blade.php
+++ resources/views/layouts/navbar/header.blade.php
@@ -2,7 +2,7 @@
     <div class="flex h-16">
             <a href="/" class="flex items-center gap-2">
                 <img class="w-15" src="{{ asset('images/logo-intynet.png') }}" alt="INTYNET Logo" />
-                <span class="text-3xl font-bold text-gray-800">INTYNET</span>
+                <span class="text-3xl font-bold text-gray-800">INTY.NET</span>
             </a>
     </div>
 </div>
