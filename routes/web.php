c0ce4d7941a6c5b16fe219de63a4750cea5e02b7
Yasser Arafah <yasserarafat@gmail.com>
2026-01-14 10:55:10 +0800

    "breeze"

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\costumer;
-use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::controller(CustomerController::class)->group(function () {
-    Route::get('/', 'index')->name('form.index');
-    Route::post('/', 'store')->name('form.store');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::controller(AdminController::class)->group(function () {
-    Route::get('/admin', 'index')->name('admin.index');
-    Route::get('/admin/data', 'data')->name('admin.data');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
